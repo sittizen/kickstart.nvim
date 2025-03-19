5091f7d17138af681f576e778660d92694c383e2
Simone Cittadini <simone.cittadini@AONBL3231AA.gruppomol.lcl>
2025-03-19 17:17:49 +0100

    movement keys I'm used to

--- init.lua
+++ init.lua
@@ -180,10 +180,17 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
 -- TIP: Disable arrow keys in normal mode
--- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
--- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
--- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
--- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+vim.keymap.set('n', '<left>', '<cmd>echo "Use j to move!!"<CR>')
+vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+vim.keymap.set('n', '<up>', '<cmd>echo "Use i to move!!"<CR>')
+vim.keymap.set('n', '<down>', '<cmd>echo "Use k to move!!"<CR>')
+
+-- Arrow keys to what I'm used to
+vim.keymap.set('n', 'h', 'I')
+vim.keymap.set('n', 'i', '<up>')
+vim.keymap.set('n', 'j', '<left>')
+vim.keymap.set('n', 'k', '<down>')
+vim.keymap.set('n', 'l', '<right>')
 
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
